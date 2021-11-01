12af76f1e21cb67da37a0e667cb6d11866aa2541
samurai <panvovandrik@gmail.com>
2021-11-01 11:45:47 +0200

    add navigation component and fragments

---
+++ app/src/main/java/com/example/weatherapi/presentation/search/SearchFragment.kt
@@ -0,0 +1,29 @@
+package com.example.weatherapi.presentation.search
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.weatherapi.R
+import com.example.weatherapi.databinding.FragmentSearchBinding
+
+class SearchFragment : Fragment() {
+
+    private lateinit var binding: FragmentSearchBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentSearchBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.root.setOnClickListener {
+            findNavController().navigate(R.id.searchFragment_cityFragment)
+        }
+    }
+}
